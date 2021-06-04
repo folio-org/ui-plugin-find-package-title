b36bca53773b04c91ee062e6901cf8f4f56da518
Vladyslav Velytskyi <Vladyslav_Velytskyi@epam.com>
2021-06-04 08:35:04 +0300

    UIPFPAT-35: Remove react imports

--- src/components/SearchModal/SearchModal.js
+++ src/components/SearchModal/SearchModal.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import {
+  useState,
+  useEffect,
+} from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import {
